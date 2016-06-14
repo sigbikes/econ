56c43d00be4fb442e05f204f86fb2efadd4232e6
Sig <sigbikes@gmail.com>
2016-06-14 07:06:12 -0500

    2016/06/14 - JTS - Created intial website per Tango with Django

---
+++ c_growth/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    # A random dictionary because that's what Rango did
+    #WWRD
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage':"Compound Growth works!"}
+
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render(request, 'c_growth/index.html', context=context_dict)
