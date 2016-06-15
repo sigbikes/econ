aa76b560af5cad811d6955dd490c377b5003dbc1
Sig <sigbikes@gmail.com>
2016-06-15 07:09:18 -0500

    2016/06/15 - JTS - Models made and superuser added

--- c_growth/admin.py
+++ c_growth/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from c_growth.models import Run, Data
 
 # Register your models here.
+
+
+admin.site.register(Data)
+admin.site.register(Run)
--- c_growth/models.py
+++ c_growth/models.py
@@ -3,3 +3,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Data(models.Model):
+    principle = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
+    rate = models.DecimalField(default = 0.05, decimal_places = 3, max_digits = 5)
+    compoundings = models.IntegerField(default = 1)
+    year = models.DecimalField(default = 0, decimal_places = 3, max_digits = 6)
+    interest = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
+    total = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
+
+    def __str__(self):
+        return self.name
+    def __unicode__(self):
+        return self.name
+
+class Run(models.Model):
+    description = models.CharField(max_length = 256)
+    date = models.DateField(auto_now_add = True)
+    data = models.ForeignKey(Data)
+
+    def __str__(self):
+        return self.name
+    def __unicode__(self):
+        return self.name
