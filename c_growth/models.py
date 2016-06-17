02e298f740f9a068b5ee7738f342c934a52c2c7b
Sig <sigbikes@gmail.com>
2016-06-17 16:16:14 -0500

    2016/06/17 - JTS - Reverted to Old Model for Simple Testing

--- c_growth/models.py
+++ c_growth/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 # Create your models here.
 
-
 class Data(models.Model):
+    name = models.CharField(max_length = 128, default = 'Blank')
     principle = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
     rate = models.DecimalField(default = 0.05, decimal_places = 3, max_digits = 5)
     compoundings = models.IntegerField(default = 1)
@@ -13,17 +13,22 @@ class Data(models.Model):
     interest = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
     total = models.DecimalField(default = 0, decimal_places = 2, max_digits = 10)
 
+
+    class Meta:
+        verbose_name_plural = "Data"
+
     def __str__(self):
         return self.name
     def __unicode__(self):
         return self.name
 
 class Run(models.Model):
-    description = models.CharField(max_length = 256)
+    name = models.CharField(max_length = 128, default = 'Blank')
+    description = models.CharField(max_length = 256, null = True)
     date = models.DateField(auto_now_add = True)
     data = models.ForeignKey(Data)
 
     def __str__(self):
-        return self.name
+        return self.description
     def __unicode__(self):
-        return self.name
+        return self.description
